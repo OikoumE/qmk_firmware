bbbc896da364cdb94a89b8f6ffe4f011a194d428
OikoumE <83828323+OikoumE@users.noreply.github.com>
2022-08-12 00:44:49 +0200

    new layout

--- keyboards/macro_pad/keymaps/macro_pad_keymap/keymap.c
+++ keyboards/macro_pad/keymaps/macro_pad_keymap/keymap.c
@@ -6,17 +6,26 @@ enum layer_names{
     LAYERNAME
 };
 
+enum {
+    TD_ESC_CAPS,
+    
+};
 
+// Tap Dance definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for Escape, twice for Caps Lock
+    [TD_ESC_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_CAPS),
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_A, KC_B, KC_C, KC_D, KC_E, KC_F, KC_G, 
-        KC_H, KC_I, KC_J, KC_K, KC_L, KC_M, KC_N, 
-        KC_O, KC_P, KC_Q, KC_R, KC_S, KC_T, KC_U
+        KC_TAB, KC_ESCAPE, KC_A, KC_Q, KC_E, KC_F, KC_J, 
+        KC_I, KC_F, KC_H, KC_LEFT_CTRL, KC_LEFT_SHIFT, KC_LEFT_ALT, KC_S, 
+        KC_G, KC_P, KC_M, KC_X, KC_Z, KC_Y
     ),
     [1] = LAYOUT(
-        KC_A, KC_B, KC_C, KC_D, KC_E, KC_F, KC_G, 
-        KC_H, KC_I, KC_J, KC_K, KC_L, KC_M, KC_N, 
-        KC_O, KC_P, KC_Q, KC_R, KC_S, KC_T, KC_U
-    )
+        KC_TAB, KC_ESCAPE, KC_A, KC_Q, KC_E, KC_F, KC_J, 
+        KC_I, KC_F, KC_H, KC_LEFT_CTRL, KC_LEFT_SHIFT, KC_LEFT_ALT, KC_S, 
+        KC_G, KC_P, KC_M, KC_X, KC_Z, KC_Y
+    ),
 };
--- keyboards/macro_pad/rules.mk
+++ keyboards/macro_pad/rules.mk
@@ -1,1 +1,3 @@
 # This file intentionally left blank
+F_CPU = 8000000
+TAP_DANCE_ENABLE = yes
